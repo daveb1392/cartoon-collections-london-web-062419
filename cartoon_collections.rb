fb697f1f5246cd727ad171f4e14861c1700301e0
github username <github email address>
2019-06-11 19:32:12 +0100

    cartoon collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index| 
+    puts "#{index + 1}. #{name} " #index + 1 to start from 1. 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(veggies)
+  veggies.map do |value|
+  "#{value.capitalize}!" #capitalize first letter of each value
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(calls)
+  calls.any? do |word| #any returns if any of the elements contained after do length is lower than 4.
+    word.length > 4
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese)
+cheese.find do |fromage|
+fromage == "cheddar" || fromage == "gouda" || fromage == "camembert" #need to establish a list of items to search for, but what happens if I have a massive dataset aka array??I'll figure it out.
+end
 end
